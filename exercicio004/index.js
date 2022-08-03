da4d1cf1a1a5fa2f5155adc3c2598bf52c3ff7fb
leonardo cândido12 <leonardocandido1211@gmail.com>
2022-08-03 17:00:04 -0300

    exercicio de swifth

---
+++ exercicio004/index.js
@@ -0,0 +1,24 @@
+let DistaceIY = prompt("informe a distância em anos-luz.")
+
+let ChooseOption = prompt(
+    "Informe a nova unidade de medida :"+"\n1- Parsec(pc)"
+    +"\n2- unidade astronómica(AU)"
+    +"\n3- quilómetros(km)")
+ switch(ChooseOption){
+    case "1":
+        let DistancePC = ChooseOption * 0.306601
+        alert("distancia em Anos-Luz: "+ChooseOption+" l.y"
+        +"\nconversão desejada: "+DistancePC+" parsec")
+    break
+    case "2":
+        let DistaceAU = ChooseOption * 63241.1
+        alert("distancia em Anos-Luz: "+ ChooseOption+" i.y"+"\nconversão desejada: "+DistaceAU+" AU")
+    break    
+    case "3":
+        let DistanceKM = ChooseOption * 9.5 * Math.pow(10,12)
+        alert("distancia em Anos-Luz: "+ChooseOption+" l.y"+"\nconversão desejada: "+DistanceKM+" Km")
+    break    
+    default:
+        alert("distancia em anos-luz: "+ChooseOption+" l.y"+"\nunidade não identificada: conversão fora do escopo")
+ }
+
